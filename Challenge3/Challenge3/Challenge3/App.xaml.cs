2f75bd3cc0d8605c516352d5086dc32dac080c0e
Unknown <s090729@ap.be>
2017-03-23 21:49:55 +0100

    Challenge 3, niet af
    Weet niet waar ik de code moet plaatsen om de GET request te doen om de JSON in te laden. Als ik dit in het bestand App.xaml.cs wil doen herkend deze de syntax "GET",... niet.

---
+++ Challenge3/Challenge3/Challenge3/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Challenge3
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new Challenge3.MainPage();
+
+            string mobileServiceClientUrl = "http://xamarinalliancebackend.azurewebsites.net";
+            MobileServiceClient Client = new MobileServiceClient(mobileServiceClientUrl);
+
+            
+        }
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+            
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+
+
+	}
+}
---
+++ Challenge3/Challenge3/Challenge3/Weapon.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenge3
+{
+    public class Weapon
+    {
+        Int32 id;
+        string name;
+        string description;
+        ICollection<Character> characters;
+
+        [JsonProperty(PropertyName = "id")]
+        public Int32 Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public string Version { get; set; }
+    }
+}
